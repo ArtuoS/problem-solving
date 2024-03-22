f2dd5e58c109915b4469410b044f3523d15b2f19
Arthur Molinari <arthur.molinari@lincros.com>
2024-03-21 21:30:29 -0300

    🔥 Leetcode problem solving.

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 
-	searchinsertposition "github.com/ArtuoS/problem-solving/search_insert_position"
+	rangesumqueryimmutable "github.com/ArtuoS/problem-solving/range_sum_query_immutable"
 )
 
 func main() {
-	fmt.Println(searchinsertposition.SearchInsert([]int{1, 3, 5, 6}, 5))
-	fmt.Println(searchinsertposition.SearchInsert([]int{1, 3, 5, 6}, 2))
-	fmt.Println(searchinsertposition.SearchInsert([]int{1, 3, 5, 6}, 7))
+	numArray := rangesumqueryimmutable.Constructor([]int{-2, 0, 3, -5, 2, -1})
+	fmt.Println(numArray.SumRange(0, 2))
+	fmt.Println(numArray.SumRange(2, 5))
+	fmt.Println(numArray.SumRange(0, 5))
 }
